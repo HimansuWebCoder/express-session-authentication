0c4184105e2d9b564d9461695135bdc91be534f7
himansu naik <naikhimansu546@gmail.com>
2024-11-06 22:14:39 +0530

    database added

--- server/server.js
+++ server/server.js
@@ -2,7 +2,21 @@ const express = require('express');
 const app = express();
 const session = require('express-session');
 const cors = require('cors');
+const knex = require("knex");
 const PORT = 3000;
+
+const db = knex({
+     client: 'pg',
+  connection: {
+    host: "localhost",
+    port: 5432,
+    user: "postgres",
+    database: "todos",
+    password: "test",
+}
+
+})
+
 
 app.use(session({
   secret: '*****',
@@ -36,14 +50,24 @@ app.post('/signup', (req, res) => {
    return res.json("signup successfully");
 })
 
+app.post("/todos", (req, res) => {
+    const {todo} = req.body;
+    db("todos").insert({title: todo}).returning("*")
+        .then(data => {
+            return res.json(data);
+        })
+})
+
 app.post('/login', (req, res) => {
-    const { username} = req.body;
-    req.session.username = username;
-    console.log(req.session.username); 
+    const { todo } = req.body;
+    req.session.todo = todo;
+    // req.session.username = username;
+    // console.log(req.session.username); 
 
-    const findUserData = myData.find(data => req.session.username === data.username );
+    // const findUserData = myData.find(data => req.session.username === data.username );
+    const findTodo = db("todos").where({title: req.session.todo}).first();
 
-    if (findUserData) {
+    if (findTodo) {
         // return res.json(findUserData)
         return res.json("Login successfully")
     } else {
@@ -52,15 +76,23 @@ app.post('/login', (req, res) => {
 })
 
 app.get('/data', (req, res) => {
-    const findUserData = myData.find(data => req.session.username === data.username);
-
-    if(findUserData) {
-        console.log(findUserData)
-        return res.json(findUserData)
-    } else {
-        return res.json("need login")
+     if (!req.session.todo) {
+        return res.json("Need to log in or Invalid todo");
     }
-})
+
+    db("todos")
+        .where({ title: req.session.todo })
+        .first()
+        .then(findTodo => {
+            if (findTodo) {
+                console.log(findTodo.title);
+                return res.json(findTodo.title);
+            } else {
+                return res.json("need login");
+            }
+        })
+});
+
 
 
 app.listen(PORT, () => {
